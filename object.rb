7f505230201f12e8e95da6f5d37684725c6a7cc0
benoit <torrentdu64@gmail.com>
2024-08-14 22:23:20 +1200

    Create object.rb

---
+++ object.rb
@@ -0,0 +1,19 @@
+module Services
+  class Object
+    class << self
+      def call(*arg)
+        new(*arg).constructor
+      end
+    end
+
+    attr_reader :result
+    def constructor
+      @result = call
+      self
+    end
+
+    def call
+      fail NotImplementedError unless defined?(super)
+    end
+  end
+end
